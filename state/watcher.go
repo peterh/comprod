5d92b37b7941579dc89b5be21163645a03e12211
Peter Harris <git@peter.is-a-geek.org>
2012-04-14 14:37:44 -0400

    Update market once per day

--- state/watcher.go
+++ state/watcher.go
@@ -3,6 +3,8 @@ package state
 import (
 	"encoding/gob"
 	"log"
+	"math"
+	"math/rand"
 	"os"
 	"os/signal"
 	"time"
@@ -33,8 +35,45 @@ func (g *Game) write(fn string) {
 	os.Remove(old)
 }
 
+func nextTurn() <-chan time.Time {
+	now := time.Now().UTC()
+	tomorrow := now.Add(time.Hour * 23)
+	for tomorrow.Day() == now.Day() {
+		tomorrow = tomorrow.Add(time.Hour)
+	}
+	next := time.Date(tomorrow.Year(), tomorrow.Month(), tomorrow.Day(), 0, 0, 0, 0, time.UTC)
+	return time.After(next.Sub(now))
+}
+
+func (g *Game) newDay() {
+	const rounds = 15
+	const (
+		up = iota
+		down
+		dividend
+	)
+
+	for i := 0; i < rounds; i++ {
+		adjust := uint64(math.Pow(rand.Float64()*.8+1.2, 5.0))
+		stock := rand.Intn(stockTypes)
+		switch rand.Intn(3) {
+		case up:
+			g.g.Stock[stock].Value += adjust
+		case down:
+			g.g.Stock[stock].Value -= adjust
+		case dividend:
+			if g.g.Stock[stock].Value >= startingValue {
+				for _, p := range g.g.Player {
+					p.Cash += adjust * p.Shares[stock]
+				}
+			}
+		}
+	}
+}
+
 func watcher(g *Game, filename string, changed chan struct{}) {
 	var tick <-chan time.Time
+	tock := nextTurn()
 
 	sigint := make(chan os.Signal, 1)
 	signal.Notify(sigint, os.Interrupt, os.Kill)
@@ -48,6 +87,10 @@ func watcher(g *Game, filename string, changed chan struct{}) {
 		case <-tick:
 			tick = nil
 			g.write(filename)
+		case <-tock:
+			g.newDay()
+			g.write(filename)
+			tock = nextTurn()
 		case <-sigint:
 			if tick != nil {
 				g.write(filename)
